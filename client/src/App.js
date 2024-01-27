53d1b6db693d4d66d60bc0ca85d68bbe24a4118c
ham144 <24434muhammad.yafizham@gmail.com>
2024-01-27 17:57:33 +0700

    backend menit 1:02

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
+import { useDispatch } from 'react-redux'
 
+import {getposts} from './actions/posts'
+import Posts from './components/Posts/Posts.js'
+import Form from './components/Form/Form.js'
 
-const App = ()=> {
-    return (    
-        <div>
-            <h1>konto</h1>
-        </div>
-    );
-
+import card from './images/card.png'
+import useStyles from './styles.js'
+const App = ()=>{
+    const classes = useStyles()
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        dispatch(getposts()) 
+    },[dispatch])
+    return (
+        <Container maxWidth="lg" >
+            <AppBar className={classes.AppBar} position='static' color='inherit'>
+                <Typography className={classes.heading} variant='h2' align='center'>MEMORIES</Typography>
+                <img className={classes.image} src={card}  alt='card'  height='70'></img>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justifyContent='space-between' alignItems='stretch' spacing={3}>
+                        <Grid item xs={12} sm={7} ><Posts/></Grid>
+                        <Grid item xs={12} sm={4} ><Form/></Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
+    )
 }
 export default App
